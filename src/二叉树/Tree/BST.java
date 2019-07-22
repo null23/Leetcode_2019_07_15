68f89b96ed812643cce1b14a2c99cb222f660387
qianji.wang <qianji.wang@11bee.com>
2019-07-22 11:27:05 +0800

    'sr'

---
+++ src/二叉树/Tree/BST.java
@@ -0,0 +1,20 @@
+package 二叉树.Tree;
+
+/**
+ * Created by qianji.wang on 2019/7/22.
+ */
+public class BST {
+    private Node root;
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    public void insert(Integer value){
+
+    }
+}
