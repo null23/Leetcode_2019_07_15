a2bbcc2171ca9b041c0067eb00fca16ec97b3358
qianji.wang <qianji.wang@11bee.com>
2019-07-22 14:57:21 +0800

    'src'

--- src/二叉树/Tree/BST.java
+++ src/二叉树/Tree/BST.java
@@ -2,6 +2,7 @@ package 二叉树.Tree;
 
 /**
  * Created by qianji.wang on 2019/7/22.
+ * 二叉查找树的插入
  */
 public class BST {
     private Node root;
@@ -14,7 +15,31 @@ public class BST {
         this.root = root;
     }
 
-    public void insert(Integer value){
+    public Node insert(Integer value){
+        if(this.root == null){
+            this.root = new Node(value);
+            return this.root;
+        }else{
+            return insert(root, value);
+        }
+    }
 
+    /**
+     * 插入新的节点
+     * 并且返回头节点
+     * 注意这里返回的头节点，这里是递归查找新节点应该插入的位置
+     */
+    public Node insert(Node node, Integer value){
+        if(node == null){
+            return new Node(value);
+        }
+        if(node.getValue().equals(value)){
+            node.setValue(value);
+        }else if(node.getValue() < value){
+            node.setLeft(insert(node.getLeft(), value));
+        }else if(node.getValue() > value){
+            node.setRight(insert(node.getLeft(), value));
+        }
+        return node;
     }
 }
