8d83865d653c8906f0e1010970a3aff6d7848a36
qianji.wang <qianji.wang@11bee.com>
2019-07-22 16:41:13 +0800

    'src'

--- src/二叉树/Tree/BST_Order.java
+++ src/二叉树/Tree/BST_Order.java
@@ -1,10 +1,16 @@
 package 二叉树.Tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by qianji.wang on 2019/7/22.
  * 深度优先遍历：前序遍历(根左右)，中序遍历(左根右)，后序遍历(左右根)
  * 注意，输出打印的其实都是根节点
  * 因为在递归到底的情况下，递归到底节点肯定是个叶子节点，是没有左右孩子的，其实这个叶子节点就是"根节点"
+ *
+ * 广度优先遍历：层序遍历
+ * 这里使用队列记录上一层的所有节点，依此把上一层节点的所有记录打印出来
  */
 public class BST_Order {
     private Node root;
@@ -39,6 +45,27 @@ public class BST_Order {
             preOrder(root.getLeft());
             preOrder(root.getRight());
             System.out.println(root.getValue());
+        }
+    }
+
+    /**
+     * 层序遍历
+     * 把上一层打印出来，然后把上一层的所有左右孩子分别加入队列
+     */
+    public void levelSort(Node root){
+        Queue<Node> queue = new LinkedList();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            Node curNode = queue.poll();
+            System.out.println(curNode);
+            //把左孩子加入队列尾部
+            if(curNode.getLeft() != null){
+                queue.offer(curNode.getLeft());
+            }
+            //把右孩子加入队列尾部
+            if(curNode.getRight() != null){
+                queue.offer(curNode.getRight());
+            }
         }
     }
 }
