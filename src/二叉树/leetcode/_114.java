1e4e37e54fb78a46882f7a515309974c3510ef80
nvl23 <3346242982@qq.com>
2019-08-14 00:27:38 +0800

    'src'

--- src/二叉树/leetcode/_114.java
+++ src/二叉树/leetcode/_114.java
@@ -28,11 +28,27 @@ import 二叉树.Tree.TreeNode;
  */
 public class _114 {
 
+    /**
+     * 思路：
+     *  可以发现展开的顺序其实就是二叉树的先序遍历。算法和 94 题 中序遍历的 Morris 算法有些神似，我们需要两步完成这道题。
+     *  1.将左子树插入到右子树的地方
+     *  2.将原来的右子树接到左子树的最右边节点
+     *  3.考虑新的右子树的根节点，一直重复上边的过程，直到新的右子树为 null
+     *
+     *  时间复杂度：O(n)
+     */
     public void flatten(TreeNode root) {
-
-    }
-
-    public void helper(){
-
+        while(root != null){
+            if(root.getLeft() != null){
+                TreeNode pre = root.getLeft();
+                while(pre.getRight() != null){
+                    pre = pre.getRight();
+                }
+                pre.setRight(root.getRight());
+                root.setRight(root.getLeft());
+                root.setLeft(null);
+            }
+            root = root.getRight();
+        }
     }
 }
---
+++ src/二叉树/leetcode/_114_2.java
@@ -0,0 +1,10 @@
+package 二叉树.leetcode;
+
+import 二叉树.Tree.TreeNode;
+
+public class _114_2 {
+
+    public void flatten(TreeNode root) {
+
+    }
+}
