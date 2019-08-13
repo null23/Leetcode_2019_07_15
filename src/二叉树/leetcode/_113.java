216578abf856881ebf2474dbb7dd097271db8381
nvl23 <3346242982@qq.com>
2019-08-13 23:31:00 +0800

    'src'

--- src/二叉树/leetcode/_113.java
+++ src/二叉树/leetcode/_113.java
@@ -1,5 +1,10 @@
 package 二叉树.leetcode;
 
+import 二叉树.Tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * MinNum
  * 求为指定和的二叉树路径（到叶子节点）
@@ -7,4 +12,35 @@ package 二叉树.leetcode;
  * 说明: 叶子节点是指没有子节点的节点。
  */
 public class _113 {
+
+    /**
+     * 思路：先序遍历 + 回溯法
+     * 时间复杂度：O(n)
+     *
+     * 回溯法：https://segmentfault.com/a/1190000018319044?utm_source=tag-newest
+     */
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> list = new ArrayList<>();
+        helper(root, sum, new ArrayList<>(), list);
+        return list;
+    }
+
+    public void helper(TreeNode root, int sum, List<Integer> path, List<List<Integer>> res){
+        if(root == null){
+            return;
+        }
+        path.add(root.getVal());
+
+        if(root.getLeft() == null && root.getRight() == null){
+            if(root.getVal() == sum){
+                res.add(new ArrayList<>(path));
+            }
+        }else{
+            helper(root.getLeft(), sum - root.getVal(), path, res);
+            helper(root.getRight(), sum - root.getVal(), path, res);
+        }
+
+        //回溯，因为Java是引用传递，不能让path当前的最后一个节点影响对以后的递归的结果造成影响
+        path.remove(path.size() - 1);
+    }
 }
---
+++ src/二叉树/leetcode/_114.java
@@ -0,0 +1,38 @@
+package 二叉树.leetcode;
+
+import 二叉树.Tree.TreeNode;
+
+/**
+ * MidNum
+ * 将二叉树展开为链表
+ *
+ * 给定一个二叉树，原地将它展开为链表。
+ * 例如，给定二叉树
+ *     1
+ *    / \
+ *   2   5
+ *  / \   \
+ * 3   4   6
+ * 将其展开为：
+ * 1
+ *  \
+ *   2
+ *    \
+ *     3
+ *      \
+ *       4
+ *        \
+ *         5
+ *          \
+ *           6
+ */
+public class _114 {
+
+    public void flatten(TreeNode root) {
+
+    }
+
+    public void helper(){
+
+    }
+}
