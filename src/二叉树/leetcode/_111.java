d488bcae0c789565d7988efae5706b62d0814575
nvl23 <3346242982@qq.com>
2019-08-13 15:19:24 +0800

    'src'

--- src/二叉树/leetcode/_111.java
+++ src/二叉树/leetcode/_111.java
@@ -4,13 +4,21 @@ import 二叉树.Tree.TreeNode;
 
 /**
  * Easy
- * 二叉树的最小深度
+ * 二叉树的最小深度（从根节点到叶子节点）
  */
 public class _111 {
 
     /**
      * 还是递归，自底向上的递归
+     * 时间复杂度：O(n)
      *
+     * 思路：
+     *  这道题有点坑，因为题目是求根节点到叶子节点的最小深度，所以之前的解法在出现(1,2)这样的测试用例就有问题。
+     *  所以这道题分为很多种情况：
+     *      1.当前节点为空，就返回0，这个没什么问题
+     *      2.当前节点的左右孩子为空，说明到达了叶子节点，返回1
+     *      3.当前节点的左孩子或右孩子有一个是空的，这时候就要避免上边提到的那个坑了，假如当前节点的左孩子是空的，那么我们就要返回（右孩子的高度 + 1），而不是返回（0 + 1）
+     *      4.当前节点的左右孩子均不为空，那就返回左右孩子中高度最小的
      */
     public int minDepth(TreeNode root) {
         if(root == null){
@@ -19,15 +27,14 @@ public class _111 {
         if(root.getLeft() == null && root.getRight() == null){
             return 1;
         }
+        int leftDepth = minDepth(root.getLeft());
+        int rightDepth = minDepth(root.getRight());
 
-        int minDepth = Integer.MAX_VALUE;
-        if(root.getLeft() != null){
-            minDepth = Math.min(minDepth(root.getLeft()), minDepth);
+        if(root.getLeft() == null || root.getRight() == null){
+            return leftDepth + rightDepth + 1;
         }
-        if(root.getLeft() != null){
-            minDepth = Math.min(minDepth(root.getRight()), minDepth);
-        }
-        return minDepth + 1;
+
+        return Math.min(leftDepth, rightDepth) + 1;
     }
 
 }
